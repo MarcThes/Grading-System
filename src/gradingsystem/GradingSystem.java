0267ed9d48c71f03203a2ad6c762ad2f20b5f481
MarcThes <114109147+MarcThes@users.noreply.github.com>
2023-08-02 08:12:25 -0400

    Update GradingSystem.java

--- src/gradingsystem/GradingSystem.java
+++ src/gradingsystem/GradingSystem.java
@@ -1,64 +1,104 @@
-import java.util.Scanner;
+package gradingSystem;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import gradingSystem.GradeLetter;
+
+/**
+ * @author Marc T
+ * 
+ *         This class represents a grading system.
+ */
 public class GradingSystem {
 
-	
-  private static final int MAX_STUDENTS = 10;
-  private static final int PASSING_GRADE = 60;
-  
-  public enum GradeLetter {
-    A, B, C, D, F
-  }
+	private static final int MAX_STUDENTS = 10;
+	private static final int PASSING_GRADE = 60;
+	private static Scanner scanner = new Scanner(System.in);
 
-  public static void main(String[] args) {
+	public static List<Integer> getGradesFromUser(int numGrades) {
+		List<Integer> grades = new ArrayList<>();
+		for (int i = 0; i < numGrades; i++) {
+			grades.add(getExamGradeFromUser());
+		}
+		return grades;
+	}
 
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Entrez le nombre d'étudiants (max "+MAX_STUDENTS+") : ");
-    int numStudents = scanner.nextInt();
-    scanner.nextLine();
-    
-    if (numStudents > MAX_STUDENTS) {
-      System.out.println("Nombre d'étudiants trop grand, réglé sur "+MAX_STUDENTS);
-      numStudents = MAX_STUDENTS;
-    }
-    
-    Student[] students = new Student[numStudents];
+	/**
+	 * Main method for the grading system.
+	 * 
+	 * @param args command line arguments
+	 */
+	public static void main(String[] args) {
 
-    // Saisie des infos étudiants
-    for (int i = 0; i < numStudents; i++) {
-      
-      System.out.print("Entrez le nom : ");
-      String lastName = scanner.nextLine();
+		int numStudents = 0;
+		boolean isValid;
+		do {
+			isValid = true;
+			System.out.print("Enter the number of students (max " + MAX_STUDENTS + ") : ");
+			try {
+				numStudents = Integer.parseInt(scanner.nextLine());
+				if (numStudents < 1 || numStudents > MAX_STUDENTS) {
+					System.out
+							.println("Invalid number of students. Please enter a number between 1 and " + MAX_STUDENTS);
+					isValid = false;
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid input. Please enter a number.");
+				isValid = false;
+			}
+		} while (!isValid);
 
-      System.out.print("Entrez le prénom : ");
-      String firstName = scanner.nextLine();
+		Student[] students = new Student[numStudents];
 
-      int grade1 = getExamGradeFromUser(scanner);
-      int grade2 = getExamGradeFromUser(scanner); 
-      int grade3 = getExamGradeFromUser(scanner);
+		// Input student info
+		for (int i = 0; i < numStudents; i++) {
 
-      students[i] = new Student(lastName, firstName, grade1, grade2, grade3);
-    }
+			System.out.print("Enter the last name : ");
+			String lastName = scanner.nextLine();
 
-    // Affichage des résultats
-    for (Student student : students) {
-      System.out.printf("Nom : %s %s - Moyenne : %.2f - Note : %s%n",
-        student.getLastName(), student.getFirstName(),
-        student.calculateAverageGrade(), student.getGradeLetter());
-    }
-    
-    scanner.close();
-  }
+			System.out.print("Enter the first name : ");
+			String firstName = scanner.nextLine();
 
-  public static int getExamGradeFromUser(Scanner scanner) {
-    
-    int grade;
-    do {
-      System.out.print("Entrez la note : ");
-      grade = scanner.nextInt();
-    } while (grade < 0 || grade > 100); 
+			List<Integer> studentGrades = getGradesFromUser(3);
+			students[i] = new Student(lastName, firstName, studentGrades.get(0), studentGrades.get(1),
+					studentGrades.get(2));
 
-    return grade;
-  }
+		}
+
+		// Display results
+		for (Student student : students) {
+		    double averageGrade = student.calculateAverageGrade();
+		    GradeLetter gradeLetter = student.getGradeLetter();
+		    System.out.printf("Name : %s %s - Average : %.2f - Grade : %s", 
+		                      student.getLastName(),
+		                      student.getFirstName(),
+		                      averageGrade,
+		                      gradeLetter);
+		    if (averageGrade >= PASSING_GRADE) {
+		        System.out.print(" - Success!");
+		    }
+		    System.out.println();
+		}
+	}
+
+	/**
+	 * Method to get the exam grade from the user.
+	 * 
+	 * @param scanner Scanner object to read user input
+	 * @return the grade entered by the user
+	 */
+	public static int getExamGradeFromUser() {
+
+		int grade;
+		do {
+			System.out.print("Enter the grade : ");
+			grade = scanner.nextInt();
+			scanner.nextLine();
+		} while (grade < 0 || grade > 100);
+
+		return grade;
+	}
 
 }
